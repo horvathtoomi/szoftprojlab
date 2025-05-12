c813485b1e89b98b5e346f3d1ed02b0933612138
Horváth Tamás <tamas.horvath916@gmail.com>
2025-05-12 17:29:35 +0200

    Kis alakitasok

--- src/main/java/mushroom/GeometryString.java
+++ src/main/java/mushroom/GeometryString.java
@@ -9,9 +9,12 @@ public class GeometryString extends Geometry implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
     private final int width;
+    private int endX, endY;
 
-    public GeometryString(int x, int y, int width) {
+    public GeometryString(int x, int y, int x2, int y2, int width) { // Az x, y lesz a kezdetének a koordinátái, az endX és endY pedig a végének.
         super(x,y);
+        endX = x2;
+        endY = y2;
         this.width = width;
     }
 
