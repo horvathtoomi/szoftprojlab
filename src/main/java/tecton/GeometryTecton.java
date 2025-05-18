7d0e27de51fa27b5dd1a26c1ff95c533c9a4acf8
Nagy László Zsolt <platoniccat0253@gmail.com>
2025-05-18 15:30:26 +0200

    Maradék javadoc

--- src/main/java/tecton/GeometryTecton.java
+++ src/main/java/tecton/GeometryTecton.java
@@ -5,16 +5,26 @@ import main.java.Geometry;
 import java.io.Serial;
 import java.io.Serializable;
 
+/**
+ * A Geometry osztály kiterjesztése tektonokra úgy, hogy eltárolja a tekton sugarát is
+ */
 public class GeometryTecton extends Geometry implements Serializable {
 	@Serial
 	private static final long serialVersionUID = 1L;
 	private final int radius;
-	
+
+	/**
+	 * Létrehoz egy új példányt a kapott paraméterekkel
+	 * @param x X koordináta
+	 * @param y Y koordináta
+	 * @param radius a tekton sugara
+	 */
 	public GeometryTecton(int x, int y, int radius) {
 		super(x, y);
 		this.radius = radius;
 	}
-	
+
+	//Getter
     public int getRadius() {
 		return radius;
 	}
